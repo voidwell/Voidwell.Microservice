49b213f7484214b2c8fbea247d2b29da500524d6
Jess Tremper <jess@voidwell.com>
2023-03-08 15:10:49 -0500

    Fix bad framework usage

--- src/Voidwell.Microservice/EntityFramework/DbSetExtensions.cs
+++ src/Voidwell.Microservice/EntityFramework/DbSetExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Voidwell.Microservice.EntityFramework
@@ -18,14 +21,21 @@ namespace Voidwell.Microservice.EntityFramework
         //
         // Summary:
         //     If a match is found, only update properties that are not set to null in the new passed entity
-        public static async Task<TEntity> UpsertWithoutNullPropertiesAsync<TEntity>(this DbContext ctx, TEntity entity)
+        public static async Task<TEntity> UpsertWithoutNullPropertiesAsync<TEntity>(this DbSet<TEntity> dbSet, TEntity entity, Expression<Func<TEntity, bool>> searchPredicate)
             where TEntity : class
         {
-            await ctx.Upsert(entity)
-                .WhenMatched((existingRec, newRec) => AssignNonNullProperties(existingRec, newRec))
-                .RunAsync();
+            var storeEntity = await dbSet.Where(searchPredicate).FirstOrDefaultAsync();
 
-            return entity;
+            if (storeEntity == null)
+            {
+                await dbSet.AddAsync(entity);
+                return entity;
+            }
+            else
+            {
+                var preparedEntity = PrepareEntityUpdate(dbSet, storeEntity, entity);
+                return preparedEntity;
+            }
         }
 
         public static async Task<IEnumerable<TEntity>> UpsertAsync<TEntity>(this DbContext ctx, IEnumerable<TEntity> entities)
@@ -40,21 +50,42 @@ namespace Voidwell.Microservice.EntityFramework
         //
         // Summary:
         //     If a match is found, only update properties that are not set to null in the new passed entity
-        public static async Task<IEnumerable<TEntity>> UpsertWithoutNullPropertiesAsync<TEntity>(this DbContext ctx, IEnumerable<TEntity> entities)
+        public static async Task<IEnumerable<TEntity>> UpsertWithoutNullPropertiesAsync<TEntity>(this DbSet<TEntity> dbSet, IEnumerable<TEntity> entities, Expression<Func<TEntity, bool>> searchPredicate, Func<TEntity, TEntity, bool> matchPredicate)
             where TEntity : class
         {
-            await ctx.UpsertRange(entities)
-                .WhenMatched((existingRec, newRec) => AssignNonNullProperties(existingRec, newRec))
-                .RunAsync();
+            var result = new List<TEntity>();
+            var createdEntities = new List<TEntity>();
 
-            return entities;
+            var storedEntities = await (searchPredicate != null ? dbSet.Where(searchPredicate) : dbSet).ToListAsync();
+
+            foreach (var entity in entities)
+            {
+                var storeEntity = storedEntities.FirstOrDefault(storedEntity => matchPredicate(storedEntity, entity));
+                if (storeEntity == null)
+                {
+                    createdEntities.Add(entity);
+                }
+                else
+                {
+                    var preparedEntity = PrepareEntityUpdate(dbSet, storeEntity, entity);
+                    result.Add(preparedEntity);
+                }
+            }
+
+            if (createdEntities.Any())
+            {
+                await dbSet.AddRangeAsync(createdEntities);
+                result.AddRange(createdEntities);
+            }
+
+            return result;
         }
 
-        private static TEntity AssignNonNullProperties<TEntity>(TEntity target, TEntity source) where TEntity : class
+        private static T PrepareEntityUpdate<T>(DbSet<T> dbSet, T target, T source) where T : class
         {
-            foreach (var fromProp in typeof(TEntity).GetProperties())
+            foreach (var fromProp in typeof(T).GetProperties())
             {
-                var toProp = typeof(TEntity).GetProperty(fromProp.Name);
+                var toProp = typeof(T).GetProperty(fromProp.Name);
                 var toValue = toProp.GetValue(source, null);
                 if (toValue != null)
                 {
@@ -62,6 +93,7 @@ namespace Voidwell.Microservice.EntityFramework
                 }
             }
 
+            dbSet.Update(target);
             return target;
         }
     }
